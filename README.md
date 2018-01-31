ce71569bb6dee282d81ddff16e366dc1a5678d8d
Han Li <Han.Li@capitalone.com>
2018-01-31 15:56:00 -0500

    add nodemon and --inspect for chroem debug

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## graphql, koa, typeorm
 A graphql web server for ESA 
+ 
+Run `yarn start:watch` for local development. And use chrome to debug as nodemon runs node with `--inspect` already. [some debug doc](https://jondowdle.com/2017/10/node-debugger/)
+
--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,10 @@ import * as Koa from 'koa';
 import { routes } from './routes';
 import { initDatabase } from './data-source';
 
-
 (async () => {
   initDatabase()
   const app = new Koa();
   app.use(routes.routes())
     .use(routes.allowedMethods())
     .listen(4000);
-
 })();
