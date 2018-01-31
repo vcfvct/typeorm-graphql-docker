45a06aaab3920f91a0f914404fd44aae2f50e553
Han Li <Han.Li@capitalone.com>
2018-01-31 14:14:15 -0500

    ignore dist

--- dist/gq-typedef/market-types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MarketType = `
-  type Market{
-    id: Int
-    name: String
-  }
-`;
-//# sourceMappingURL=market-types.js.map
